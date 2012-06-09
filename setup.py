40af82e0882e011b287e185ee820b3ded5acfb33
David Novakovic <davidnovakovic@gmail.com>
2012-06-09 11:40:41 +0200

    A setup.py for it.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='tornado_mobile_google_analytics',
+      version='1.0',
+      description='Google Mobile Web Analytics Module for Tornado',
+      author='dpnova',
+      author_email='dpn@dpn.name',
+      url='https://github.com/dpnova/tornado-mobile-google-analytics',
+      py_modules=['tornadomobilega',]
+     )
